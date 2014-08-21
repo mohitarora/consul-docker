77c5831825d3d0ba9e708bb46f7f7ad496dacd6b
marora <mailtomohitarora@gmail.com>
2014-08-20 23:51:51 -0400

    Added Application Dockerfile

---
+++ application/app.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -eo pipefail
+
+echo "[sample-app] Starting Application"
+java -jar /application/dropwizard-sample-1.0-SNAPSHOT.jar server /application/sample.yml
+
+echo "Starting consul agent"
+
+consul agent -config-dir=/config -join=172.17.0.5
