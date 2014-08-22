407c995c4ac5d450e85cef5408b4947183edaaea
marora <mailtomohitarora@gmail.com>
2014-08-22 16:11:01 -0400

    Added docs

--- application/app.sh
+++ application/app.sh
@@ -5,6 +5,4 @@ set -eo pipefail
 echo "[sample-app] Starting Application"
 java -jar /application/dropwizard-sample-1.0-SNAPSHOT.jar server /application/sample.yml &
 
-echo "Starting consul agent"
-
 /bin/launch.sh "$@"
