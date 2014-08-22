476ca2344b0cf7d5db0e1a9bbb2ac2bce5362aee
marora <mailtomohitarora@gmail.com>
2014-08-22 18:39:15 -0400

    Added delay for consul to start

--- nginx/consul-watch-config/watch.sh
+++ nginx/consul-watch-config/watch.sh
@@ -8,5 +8,8 @@ echo "Starting consul agent"
 
 consul agent -config-dir=/config "$@" &
 
+# Being optimistic that consul will start in 30s
+sleep 30
+
 echo "[sample-app] Starting Consul-Watch"
 consul-watch -f /consul-watch/config/nginx-watch.json
